10ec0f6fd3b600c1bd6661769dfe712b7af8262a
Star Brilliant <m13253@hotmail.com>
2014-06-24 23:12:08 +0800

    Add hturlparser.py

--- httpstunnel/hturlparser.py
+++ httpstunnel/hturlparser.py
@@ -3,6 +3,7 @@ import logging
 import urllib.parse
 import urllib.request
 
+
 class HTURLParser:
     def __init__(self, url):
         '''Results:
@@ -18,34 +19,38 @@ class HTURLParser:
         connect_port: The port number to be connected
         connect_request: The second parameter to be sent when HTTP connection is set up
         '''
-        urlsplit = urllib.parse.urlsplit(url, scheme='http')
-        self.scheme = urlsplit.scheme
-        self.hostname = urlsplit.hostname
-        if urlsplit.port is None:
-            self.port = 443 if urlsplit.scheme == 'https' else 80
-        else:
-            self.port = urlsplit.port
-        self.vhost = urlsplit.netloc
-        self.path = urlsplit.path or '/'
-        self.query = urlsplit.query
-        self.request = self.path+('?'+self.query if self.query else '')
-        self.proxy = False
-        if not urllib.request.proxy_bypass(urlsplit.hostname):
-            self._testproxy(urlsplit)
-        if self.proxy:
-            self.connect_hostname = self.proxy_hostname
-            self.connect_port = self.proxy_port
-            if self.scheme == 'https':
-                if ':' in self.hostname:
-                    self.connect_request = '[%s]:%d' % (self.hostname, self.port)
-                else:
-                    self.connect_request = ':'.join((self.hostname, self.port))
+        self.url = url
+        try:
+            urlsplit = urllib.parse.urlsplit(url, scheme='http')
+            self.scheme = urlsplit.scheme
+            self.hostname = urlsplit.hostname
+            if urlsplit.port is None:
+                self.port = 443 if urlsplit.scheme == 'https' else 80
             else:
-                self.connect_request = urllib.parse.urlunsplit(urlsplit[:4]+('',))
-        else:
-            self.connect_hostname = self.hostname
-            self.connect_port = self.port
-            self.connect_request = self.request
+                self.port = urlsplit.port
+            self.vhost = urlsplit.netloc
+            self.path = urlsplit.path or '/'
+            self.query = urlsplit.query
+            self.request = self.path+('?'+self.query if self.query else '')
+            self.proxy = False
+            if not urllib.request.proxy_bypass(self.hostname):
+                self._testproxy(urlsplit)
+            if self.proxy:
+                self.connect_hostname = self.proxy_hostname
+                self.connect_port = self.proxy_port
+                if self.scheme == 'https':
+                    if ':' in self.hostname:
+                        self.connect_request = '[%s]:%d' % (self.hostname, self.port)
+                    else:
+                        self.connect_request = ':'.join((self.hostname, self.port))
+                else:
+                    self.connect_request = urllib.parse.urlunsplit(urlsplit[:4]+('',))
+            else:
+                self.connect_hostname = self.hostname
+                self.connect_port = self.port
+                self.connect_request = self.request
+        except (AttributeError, IndexError, TypeError, ValueError) as e:
+            raise ValueError('Invalid URL: %s' % url) from e
 
     def _testproxy(self, urlsplit):
         self.proxy = False
@@ -69,3 +74,9 @@ class HTURLParser:
                 logging.error('Can not use proxy %s' % proxy)
         except KeyError:
             pass
+
+    def __str__(self):
+        return self.url
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.url)
